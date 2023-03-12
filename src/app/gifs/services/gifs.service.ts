4d9603fc3d33de7d54c23954a54b2547e1e4f576
Bogdan Bakhmat <darkout2012@gmail.com>
2023-03-12 17:10:49 +0100

    fin de seccion

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpParams } from "@angular/common/http"
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
@@ -8,6 +8,7 @@ import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 export class GifsService {
 
   private apiKey: string = "K1dAAbxmgTkHQpIS5z2h5cVt0ueobWjJ";
+  private servicioUrl: string = "https://api.giphy.com/v1/gifs";
   private _historial: string[] = [];
 
   //TODO: cambiar any por su tipo correspondiente
@@ -36,9 +37,14 @@ export class GifsService {
       localStorage.setItem("historial", JSON.stringify(this._historial));
     }
     
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=K1dAAbxmgTkHQpIS5z2h5cVt0ueobWjJ&q=${query}&limit=10`)
+    const params = new HttpParams()
+    .set("api_key", this.apiKey)
+    .set("limit", "10")
+    .set("q", query);
+
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, {params: params})
       .subscribe((resp) => {
-        console.log(resp.data);
         this.resultados = resp.data;
         localStorage.setItem("resultados", JSON.stringify(this.resultados));
       }); 
